f580d3101207b5a7c9de6ae15be54cab554a0703
VINAYKUMARKUNDER <98196841+VINAYKUMARKUNDER@users.noreply.github.com>
2023-03-31 11:47:03 +0530

    project inslize

---
+++ Insurance-Management-System/src/main/java/com/vinay/InsuranceManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package com.vinay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InsuranceManagementSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InsuranceManagementSystemApplication.class, args);
+	}
+
+}
